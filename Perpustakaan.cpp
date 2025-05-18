2e9f394f66b287e4728e5d600a02162316e68e45
Agnaitaansw <agnaitaansw@gmail.com>
2025-05-18 17:00:17 +0700

    Update Perpustakaan.cpp

--- Perpustakaan.cpp
+++ Perpustakaan.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+// Struct Buku
+struct Buku {
+    int id_buku;
+    string Judul;
+    int thn_terbit;
+    string pengarang;
+    bool tersedia = true; // Tambahan atribut tersedia
+    Buku* next;
+};
